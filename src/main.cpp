fba1f0cb70f19a0be05ca484e9cc39bc838e1e2d
gabrielpcosta18 <gpc3@icomp.ufam.edu.br>
2017-08-12 13:05:19 -0400

    Atualizando leitura dos dados

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,25 @@
 
 using namespace std;
 
+int readColumn(FILE *stream, char* columnFormat, void* variable, int isEndline) {
+    int read = fscanf(stream, columnFormat, variable);
+    if(!read) {
+        if(!isEndline) 
+            read = fscanf(stream, "%[^;];", variable);
+        else read = fscanf(stream, "%[^\n]\n", variable);
+        if(!read) {
+            if(!isEndline)
+                read = fscanf(stream, ";", variable);
+            else read = fscanf(stream, "\n", variable);
+            if(!read) {
+                cout << "Parser failed" << endl;
+            }
+        }
+    }
+
+    return read;
+}
+
 int main() {
     FILE *stream = fopen("artigo.csv", "r");
     int id, citations, year;
@@ -15,36 +34,44 @@ int main() {
 
     if(stream != NULL) {
         while(true) {
-            int read = fscanf(stream, "\"%d\";\"%[^\"]\";\"%d\";\"%[^\"]\";\"%[^\"]\";\"%[^\"]\";\"%[^\"]\"\n", &id, title, &year, authors, &citations, date, snippet);
-            if(read < 7) {
-                // fscanf(stream, "\"%d\";", &id);
-                // fscanf(stream, "\"%[^\"]\";", title);
-                // fscanf(stream, "\"%d\";", &year);
-                // fscanf(stream, "\"%[^\"]\";", authors);
-                // fscanf(stream, "\"%d\";", &citations);
-                // fscanf(stream, "\"%[^\"]\";", date);
-                // fscanf(stream, "%[^\n]\n", snippet);
-                read = fscanf(stream, "\"%d\";\"%[^\"]\";\"%d\";\"%[^\"]\";\"%d\";\"%[^\"]\";", &id, title, &year, authors, &citations, date);
-                read += fscanf(stream, "%[^\n]\n", snippet);
-                cout << id << endl;
-                cout << title << endl;
-                cout << year << endl;
-                cout << authors << endl;
-                cout << citations << endl;
-                cout << date << endl;
-                cout << snippet << endl;
+            int read = 0;
+            if(!readColumn(stream, "\"%d\";", &id, 0)) {
+                cout << "Parser failed: Column title couldn't be parsed" << endl;
+                break;
             }
 
-            if(read == 0) 
-                break;
+            if(!readColumn(stream, "\"%[^\"]\";", title, 0)) {
+                title[0] = '\0';
+            }
 
+            if(!readColumn(stream, "\"%d\";", &year, 0)) {
+                year = 0;
+            }
+
+            if(!readColumn(stream, "\"%[^\"]\";", authors, 0)) {
+                authors[0] = '\0';
+            }
+
+            if(!readColumn(stream, "\"%d\";", &citations, 0)) {
+                citations = 0;
+            }
+
+            if(!readColumn(stream, "\"%[^\"]\";", date, 0)) {
+                date[0] = '\0';
+            }
+
+            if(!readColumn(stream, "%[^\n]\n", snippet, 1)) {
+                snippet[0] = '\0';
+            }
+            
+            cout << "REGISTRO" << endl;
             cout << id << endl;
-            // cout << title << endl;
-            // cout << year << endl;
-            // cout << authors << endl;
-            // cout << citations << endl;
-            // cout << date << endl;
-            // cout << snippet << endl;
+            cout << title << endl;
+            cout << year << endl;
+            cout << authors << endl;
+            cout << citations << endl;
+            cout << date << endl;
+            cout << snippet << endl << endl;
         }
     }
     return 0;
